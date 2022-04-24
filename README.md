3b11863987f3265d9782f65e75437357f6aaab96
Zhaofeng Miao <522856232@qq.com>
2022-04-24 14:30:51 +0800

    test: add test for parser

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# auth
+# Jina auth
 Pypi package (jina-auth), provide a CLI to login Jina Eco.
+
+## Development guide
+
+### Step 1: Install dependencies with extra requirements
+
+`pip install -e ".[test]"`
---
+++ tests/test_parser.py
@@ -0,0 +1,34 @@
+import pytest
+from argparse import ArgumentParser
+from auth.pasers import get_main_parser
+
+@pytest.fixture
+def parser() -> 'ArgumentParser':
+    return get_main_parser()
+
+@pytest.mark.parametrize('input,expected', [
+    (['login'], 'login'),
+    (['logout'], 'logout'),
+])
+def test_login_logout(parser: 'ArgumentParser', input, expected):
+    assert parser.parse_args(input).cli == expected
+
+
+@pytest.mark.parametrize('input,expected', [
+    (['token', 'create', 'foo'], ['token', 'create', 7, 'foo']),
+    (['token', 'create', '-e', '10', 'foo'], ['token', 'create', 10, 'foo']),
+    (['token', 'create', '--expire', '10', 'foo'], ['token', 'create', 10, 'foo']),
+])
+def test_token_create(parser: 'ArgumentParser', input, expected):
+    assert parser.parse_args(input).cli == expected[0]
+    assert parser.parse_args(input).operation == expected[1]
+    assert parser.parse_args(input).expire == expected[2]
+    assert parser.parse_args(input).name == expected[3]
+
+@pytest.mark.parametrize('input,expected', [
+    (['token', 'delete', 'foo'], ['token', 'delete', 'foo']),
+])
+def test_token_delete(parser: 'ArgumentParser', input, expected):
+    assert parser.parse_args(input).cli == expected[0]
+    assert parser.parse_args(input).operation == expected[1]
+    assert parser.parse_args(input).name == expected[2]
