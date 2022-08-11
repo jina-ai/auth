55d3352d07d17755524ad88ee7c5890a2f551d37
Han Xiao <han.xiao@jina.ai>
2022-08-11 12:01:22 +0200

    fix: clear deps

--- auth/parsers/base.py
+++ auth/parsers/base.py
@@ -1,24 +1,14 @@
 import argparse
 
-import importlib_metadata
-
 
 def set_base_parser():
-    from auth.parsers.helper import _chf, colored
-
-    __version__ = importlib_metadata.version("jina-auth")
+    from auth.parsers.helper import _chf
 
     parser = argparse.ArgumentParser(
-        description=f'Jina auth (v{colored(__version__, "green")}) helps you login in to Jina Ecosystem.',  # noqa F501
+        description=f'Jina auth helps you login in to Jina Ecosystem.',  # noqa F501
         formatter_class=_chf,
     )
-    parser.add_argument(
-        '-v',
-        '--version',
-        action='version',
-        version=__version__,
-        help='Show version',
-    )
+
     parser.add_argument(
         '--log-level',
         type=str,
